9f08bbcc3fc2734afd2226ed7ebb05f4491ed10b
Ulises Bocchio <ubocchio@ULINEWPRO.local>
2017-07-17 22:21:58 -0700

    spring boot 2 support

--- jasypt-spring-boot/src/main/java/com/ulisesbocchio/jasyptspringboot/EncryptablePropertySourceConverter.java
+++ jasypt-spring-boot/src/main/java/com/ulisesbocchio/jasyptspringboot/EncryptablePropertySourceConverter.java
@@ -4,17 +4,22 @@ import com.ulisesbocchio.jasyptspringboot.aop.EncryptablePropertySourceMethodInt
 import com.ulisesbocchio.jasyptspringboot.wrapper.EncryptableEnumerablePropertySourceWrapper;
 import com.ulisesbocchio.jasyptspringboot.wrapper.EncryptableMapPropertySourceWrapper;
 import com.ulisesbocchio.jasyptspringboot.wrapper.EncryptablePropertySourceWrapper;
+import lombok.SneakyThrows;
 import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.boot.bind.PropertyOrigin;
 import org.springframework.core.env.CommandLinePropertySource;
 import org.springframework.core.env.EnumerablePropertySource;
 import org.springframework.core.env.MapPropertySource;
 import org.springframework.core.env.PropertySource;
+
+import java.util.stream.Stream;
 
 /**
  * @author Ulises Bocchio
  */
 public class EncryptablePropertySourceConverter {
     @SuppressWarnings("unchecked")
+    @SneakyThrows
     public static <T> PropertySource<T> proxyPropertySource(PropertySource<T> propertySource, EncryptablePropertyResolver resolver) {
         //Silly Chris Beams for making CommandLinePropertySource getProperty and containsProperty methods final. Those methods
         //can't be proxied with CGLib because of it. So fallback to wrapper for Command Line Arguments only.
@@ -33,16 +38,34 @@ public class EncryptablePropertySourceConverter {
     @SuppressWarnings("unchecked")
     public static <T> PropertySource<T> instantiatePropertySource(PropertySource<T> propertySource, EncryptablePropertyResolver resolver) {
         PropertySource<T> encryptablePropertySource;
-        if (propertySource instanceof MapPropertySource) {
-            encryptablePropertySource = (PropertySource<T>) new EncryptableMapPropertySourceWrapper((MapPropertySource) propertySource, resolver);
-        } else if (propertySource.getClass().getName().equals("org.springframework.boot.context.config.ConfigFileApplicationListener$ConfigurationPropertySources")) {
-            //Some Spring Boot code actually casts property sources to this specific type so must be proxied.
+        if (needsProxyAnyway(propertySource)) {
             encryptablePropertySource = proxyPropertySource(propertySource, resolver);
+        } else if (propertySource instanceof MapPropertySource) {
+            encryptablePropertySource = (PropertySource<T>) new EncryptableMapPropertySourceWrapper((MapPropertySource) propertySource, resolver);
         } else if (propertySource instanceof EnumerablePropertySource) {
             encryptablePropertySource = new EncryptableEnumerablePropertySourceWrapper<>((EnumerablePropertySource) propertySource, resolver);
         } else {
             encryptablePropertySource = new EncryptablePropertySourceWrapper<>(propertySource, resolver);
         }
         return encryptablePropertySource;
+    }
+
+    @SuppressWarnings("unchecked")
+    private static boolean needsProxyAnyway(PropertySource<?> ps) {
+        return needsProxyAnyway((Class<? extends PropertySource<?>>) ps.getClass());
+    }
+
+    private static boolean needsProxyAnyway(Class<? extends PropertySource<?>> psClass) {
+        return needsProxyAnyway(psClass.getName());
+    }
+
+    /**
+     *  Some Spring Boot code actually casts property sources to this specific type so must be proxied.
+     */
+    private static boolean needsProxyAnyway(String className) {
+        return Stream.of(
+                "org.springframework.boot.context.config.ConfigFileApplicationListener$ConfigurationPropertySources",
+                "org.springframework.boot.context.properties.source.ConfigurationPropertySourcesPropertySource"
+                ).anyMatch(className::equals);
     }
 }

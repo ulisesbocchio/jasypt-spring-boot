1560392c6c72b49e59540f8283ebccba524c0050
Fahim Farook <fahim@fahimfarook.me>
2017-11-26 16:20:11 +0530

    Bootstrapping Jasypt for spring-cloud environments.
    Spring cloud defines a **[Bootstrap phase](http://cloud.spring.io/spring-cloud-static/spring-cloud.html#_the_bootstrap_application_context)** wherein it creates a context from configurations in boostrap.properties (or .yml) files. However it fails if Jasypt is used to decrypt encrypted text in boostrap.properties.
    <p>
    Consider the following scenario where the [config-server itself is bootstrapped](https://cloud.spring.io/spring-cloud-config/multi/multi__embedding_the_config_server.html) - so that the config-server is trying to pull configurations from config-repo during the bootstrap phase. However the password for config-repo is encrypted with Jasypt, but Jasypt decrypts the properties at a later stage only (after boostrap phase).
    
    ```
    spring.cloud.config.server.bootstrap=true
    spring.application.name=config-server
    
    spring.cloud.config.server.git.uri=https://bitbucket.org/fahim-experiment/config-repo
    
    spring.cloud.config.server.git.username=myusername@somewhere.com
    spring.cloud.config.server.git.password=ENC(GarNzpLDDoIJ3Y5lDabAllQuV74tNFbj)
    
    jasypt.encryptor.algorithm=PBEWithMD5AndDES
    jasypt.encryptor.password=passphrase
    
    ```
    
    As a solution, Jasypt can be bootstrapped in a spring-cloud based application, so that encrypted text will have decrypted by the time config-server is trying to pull from config-repo.
    
    PoC to replicate the issue [here](https://github.com/fahimfarookme/jasypt-bootstrap-issue/tree/master/issue) and the solution [here](https://github.com/fahimfarookme/jasypt-bootstrap-issue/tree/master/fix).

--- jasypt-spring-boot-starter/src/main/java/com/ulisesbocchio/jasyptspringboot/JasyptSpringBootAutoConfiguration.java
+++ jasypt-spring-boot-starter/src/main/java/com/ulisesbocchio/jasyptspringboot/JasyptSpringBootAutoConfiguration.java
@@ -1,7 +1,9 @@
 package com.ulisesbocchio.jasyptspringboot;
 
+import com.ulisesbocchio.jasyptspringboot.configuration.EnableEncryptablePropertiesBeanFactoryPostProcessor;
 import com.ulisesbocchio.jasyptspringboot.configuration.EnableEncryptablePropertiesConfiguration;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
@@ -9,6 +11,7 @@ import org.springframework.context.annotation.Import;
  * @author Ulises Bocchio
  */
 @Configuration
+@ConditionalOnMissingBean(EnableEncryptablePropertiesBeanFactoryPostProcessor.class)
 @Import(EnableEncryptablePropertiesConfiguration.class)
 public class JasyptSpringBootAutoConfiguration {
 }

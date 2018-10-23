2488951b717271c5ad34ecc18da2aad760dd8c48
Henrik Hermansson <henrik.hermansson@consultant.volvofinans.se>
2018-10-23 16:25:05 +0200

    Avoid deprecated api - prepare for Spring Boot 2.1

--- jasypt-spring-boot-starter/src/test/java/com/ulisesbocchio/jasyptspringboot/BootstrappingJasyptConfigurationTest.java
+++ jasypt-spring-boot-starter/src/test/java/com/ulisesbocchio/jasyptspringboot/BootstrappingJasyptConfigurationTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.After;
 import org.junit.Test;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
@@ -89,7 +90,7 @@ public class BootstrappingJasyptConfigurationTest {
 			final SpringApplicationBuilder builder = new SpringApplicationBuilder(BootstrapConfig.class)
 					.profiles("subversion")
 					.properties("server.port=0")
-					.web(true);
+					.web(WebApplicationType.SERVLET);
 			
 			if (listener != null) {
 				builder.listeners(listener);
@@ -116,7 +117,7 @@ public class BootstrappingJasyptConfigurationTest {
 			// order should be greater than
 			// BootstrapApplicationListener.DEFAULT_ORDER - so that this
 			// listener is invoked after BootstrapApplicationListener, otherwise
-			// bootstrap.propertis will not have been read. This is required
+			// bootstrap.properties will not have been read. This is required
 			// since encrypted text (i.e.
 			// passwords) could be configured in bootstrap.properties.
 			return BootstrapApplicationListener.DEFAULT_ORDER + 1;

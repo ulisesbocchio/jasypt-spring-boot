9513cdfa05d3ee4a7a4dc0704af0c56167d3ec93
Tomas Tulka <tomas.tulka@gmail.com>
2019-09-05 15:23:17 +0200

    environment variables loading fix

---
+++ jasypt-spring-boot/src/main/java/com/ulisesbocchio/jasyptspringboot/wrapper/EncryptableSystemEnvironmentPropertySourceWrapper.java
@@ -0,0 +1,39 @@
+package com.ulisesbocchio.jasyptspringboot.wrapper;
+
+import java.util.Map;
+
+import com.ulisesbocchio.jasyptspringboot.EncryptablePropertyFilter;
+import com.ulisesbocchio.jasyptspringboot.EncryptablePropertyResolver;
+import com.ulisesbocchio.jasyptspringboot.EncryptablePropertySource;
+import com.ulisesbocchio.jasyptspringboot.caching.CachingDelegateEncryptablePropertySource;
+
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.env.SystemEnvironmentPropertySource;
+
+/**
+ * @author Ulises Bocchio
+ */
+public class EncryptableSystemEnvironmentPropertySourceWrapper extends SystemEnvironmentPropertySource implements EncryptablePropertySource<Map<String, Object>> {
+
+    private final EncryptablePropertySource<Map<String, Object>> encryptableDelegate;
+
+    public EncryptableSystemEnvironmentPropertySourceWrapper(SystemEnvironmentPropertySource delegate, EncryptablePropertyResolver resolver, EncryptablePropertyFilter filter) {
+        super(delegate.getName(), delegate.getSource());
+        encryptableDelegate = new CachingDelegateEncryptablePropertySource<>(delegate, resolver, filter);
+    }
+
+    @Override
+    public void refresh() {
+        encryptableDelegate.refresh();
+    }
+
+    @Override
+    public Object getProperty(String name) {
+        return encryptableDelegate.getProperty(name);
+    }
+
+    @Override
+    public PropertySource<Map<String, Object>> getDelegate() {
+        return encryptableDelegate.getDelegate();
+    }
+}

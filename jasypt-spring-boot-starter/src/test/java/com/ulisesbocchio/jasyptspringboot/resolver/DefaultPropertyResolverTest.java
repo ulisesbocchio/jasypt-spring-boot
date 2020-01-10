0c2d3849e3041851e10eb25c8170c2a10c0e5682
Robert Thornton <ThorntonRP@churchofjesuschrist.org>
2020-01-10 22:19:10 +0000

    #156 allows unresolvable properties from env actuator

---
+++ jasypt-spring-boot-starter/src/test/java/com/ulisesbocchio/jasyptspringboot/resolver/DefaultPropertyResolverTest.java
@@ -0,0 +1,81 @@
+package com.ulisesbocchio.jasyptspringboot.resolver;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.io.IOException;
+import java.net.URI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpResponse;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(
+    properties = {
+        "test.username=${externalized.username}",
+        "test.password=${externalized.password}"
+    },
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class DefaultPropertyResolverTest {
+
+    @SpringBootApplication
+    public static class TestApp {
+
+    }
+
+    @Value("${spring.cloud.config.server.svn.password}")
+    public String secret;
+
+    @LocalServerPort
+    public int port;
+
+    @Autowired
+    private Environment env;
+
+    @Test
+    public void encryptedPropertyIsDecryptedInEnvironment() {
+        assertThat(env.getProperty("spring.cloud.config.server.svn.password")).isEqualTo("mypassword");
+        assertThat(secret).isEqualTo("mypassword");
+    }
+
+    @Test
+    public void propertiesAreAccessibleFromEnvActuator() {
+        ResponseEntity<?> response = new RestTemplateBuilder()
+            .errorHandler(ErrorHandler.DEFAULT)
+            .build().exchange(RequestEntity
+                .get(URI.create("http://localhost:" + port + "/actuator/env")).build(), JsonNode.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    private interface ErrorHandler extends org.springframework.web.client.ResponseErrorHandler {
+
+        ErrorHandler DEFAULT = new ErrorHandler() {
+        };
+
+        @Override
+        default boolean hasError(ClientHttpResponse response) throws IOException {
+            return false; // Don't handle errors by default
+        }
+
+        @Override
+        default void handleError(ClientHttpResponse response) throws IOException {
+            // Do nothing by default
+        }
+
+        @Override
+        default void handleError(URI url, HttpMethod method, ClientHttpResponse response) throws IOException {
+            // Do nothing by default
+        }
+    }
+}

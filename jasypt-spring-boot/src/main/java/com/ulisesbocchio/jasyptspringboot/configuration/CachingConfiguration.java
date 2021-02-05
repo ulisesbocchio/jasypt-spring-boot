9f4268048a45af073a218a420f7a43d5d5d12f32
Alastair Watts <alastair.watts@avrios.com>
2021-02-05 20:29:42 +0100

    Spring based conditional RefreshScopeRefreshedEventListener to fix thread blocking issues

--- jasypt-spring-boot/src/main/java/com/ulisesbocchio/jasyptspringboot/configuration/CachingConfiguration.java
+++ jasypt-spring-boot/src/main/java/com/ulisesbocchio/jasyptspringboot/configuration/CachingConfiguration.java
@@ -2,6 +2,7 @@ package com.ulisesbocchio.jasyptspringboot.configuration;
 
 import com.ulisesbocchio.jasyptspringboot.EncryptablePropertySourceConverter;
 import com.ulisesbocchio.jasyptspringboot.caching.RefreshScopeRefreshedEventListener;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.ConfigurableEnvironment;
@@ -9,6 +10,7 @@ import org.springframework.core.env.ConfigurableEnvironment;
 @Configuration
 public class CachingConfiguration {
     @Bean
+    @ConditionalOnClass(name = {RefreshScopeRefreshedEventListener.REFRESHED_EVENT_CLASS, RefreshScopeRefreshedEventListener.ENVIRONMENT_EVENT_CLASS})
     public RefreshScopeRefreshedEventListener refreshScopeRefreshedEventListener(ConfigurableEnvironment environment, EncryptablePropertySourceConverter converter) {
         return new RefreshScopeRefreshedEventListener(environment, converter);
     }
